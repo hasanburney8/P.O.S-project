eafbfd23eaeb4b7440250c874a3d78e9992d4e27
Basit Ali <basitali71019@gmail.com>
2023-12-04 23:09:31 +0500

    Reusable Buttons Added

--- lib/Utils/Reusable Components/button.dart
+++ lib/Utils/Reusable Components/button.dart
@@ -1,20 +1,76 @@
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
-  final String text; final Color color;
-  CustomButton({super.key, required this.text, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
+class CustomButton {
+  Widget type1(
+      {required String text,
+      required Color color,
+      required VoidCallback onTap}) {
     return InkWell(
+      onTap: onTap,
       child: Container(
-        height: 45,
-        width: 150 ,
+        height: 80,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5),
           color: color,
         ),
-        child: Center(child: Text(text, style: TextStyle(color: Colors.white, fontSize: 18),)),
+        child: Center(
+            child: Text(
+          text,
+          style: TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+        )),
+      ),
+    );
+  }
+
+  Widget type2(
+      {required String text,
+      required Color color,
+      required VoidCallback onTap}) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 45,
+        width: 180,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: color,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: Center(
+              child: Text(
+            text,
+            style: TextStyle(
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          )),
+        ),
+      ),
+    );
+  }
+
+  Widget type3(
+      {required String text,
+      required Color color,
+      required double width,
+      required double fontSize,
+      required FontWeight fontWeight,
+      required VoidCallback onTap}) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 62,
+        width: width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: color,
+        ),
+        child: Center(
+            child: Text(
+          text,
+          style: TextStyle(
+              color: Colors.white, fontSize: fontSize, fontWeight: fontWeight),
+        )),
       ),
     );
   }
