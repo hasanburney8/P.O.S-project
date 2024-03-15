b1440f9e0498dba8e2c743ab7db8e026d4c358aa
hasanburney8 <109540879+hasanburney8@users.noreply.github.com>
2024-03-16 01:33:56 +0500

    add table,add section

---
+++ lib/Utils/mycheckbox.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+//
+// class MyCheckboxListTile extends StatefulWidget {
+//   @override
+//   _MyCheckboxListTileState createState() => _MyCheckboxListTileState();
+// }
+//
+// class _MyCheckboxListTileState extends State<MyCheckboxListTile> {
+//   bool _isChecked = false;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return CheckboxListTile(
+//       title: Text('Checkbox'),
+//       value: _isChecked,
+//       onChanged: (bool newValue) {
+//         setState(() {
+//           _isChecked = newValue;
+//         });
+//       },
+//     );
+//   }
+// }
